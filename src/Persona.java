9fb2cc214f4b3d598e985c10cdfa6959d54c4d9a
Shadeonic <marks.brizaks@sk.lvt.lv>
2025-04-02 10:40:22 +0300

    Pievienoti atribūti un konstruktori abām objekta klasēm

---
+++ src/Persona.java
@@ -0,0 +1,16 @@
+public class Persona {
+//	atribūti
+private String adrese, vards, uzvards;
+private int talrunis;
+private double cena;
+private boolean sanemta_uz_vietas;
+// Konstruktors
+Persona(String adrese, String vards, String uzvards, int talrunis, double cena, boolean sanemta_uz_vietas){
+	this.adrese = adrese;
+	this.vards = vards;
+	this.uzvards = uzvards;
+	this.talrunis = talrunis;
+	this.cena = cena;
+	this.sanemta_uz_vietas = sanemta_uz_vietas;
+}
+}
---
+++ src/Pica.java
@@ -0,0 +1,14 @@
+public class Pica {
+//	atribūti
+private double lielums;
+private String piedevas, merces;
+//  Konstruktors
+Pica(double lielums, String piedevas, String merces){
+	this.lielums = lielums;
+	this.piedevas = piedevas;
+	this.merces = merces;
+}
+// Getteri
+// Setteri
+// Metodes
+}
